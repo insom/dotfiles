25894847a4d1e84cd2d140b729a16a63e582c187
Aaron Brady <aaron@insom.me.uk>
2024-06-13 22:13:32 -0400

    change to tmux TERM, basic neomutt skel, move to wezterm over kitty (for now)

---
+++ wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = 'GruvboxDark'
+config.font = wezterm.font_with_fallback {
+    { family = 'JetBrains Mono', harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, },
+    'Consolas',
+}
+
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
+config.default_cursor_style = "BlinkingBlock"
+config.font_size = 14
+config.line_height = 1.25
+config.window_background_opacity = 1
+
+return config
